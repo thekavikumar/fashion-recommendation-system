84d4c9e26205f3fc98f6189944d9327355e38d57
Kavikumar M <kavikumarceo@gmail.com>
2025-01-21 23:42:56 +0530

    modification

--- pages/TechnicalFeatures.py
+++ pages/TechnicalFeatures.py
@@ -23,15 +23,3 @@ st.markdown("#### Technical Features:")
 st.markdown("* **Object Detection Model:** Leveraged the power of the YOLOv5 model trained on fashion images to detect fashion objects in images. ")
 st.markdown("* **Feature Extraction:** Utilized a Convolutional Autoencoder implemented with PyTorch to extract latent features from detected fashion objects. ")
 st.markdown("* **Vector Index and Similarity Search Algorithm:** Implemented FAISS library to construct an index, facilitating the search for visually similar outfits based on their distinct attributes. ")
-
-# more information
-st.divider()
-st.markdown("#### Model Training and Evaluation:")
-st.markdown("If you would like to check out how I trained the model and full evaluation of each model, please check out each individual posts below:")
-st.markdown("[Object Detection Model](https://www.joankusuma.com/post/object-detection-model-yolov5-on-fashion-images) ")
-st.markdown("[Visual Search Engine](https://www.joankusuma.com/post/powering-visual-search-with-image-embedding)")
-
-# connect with me
-st.divider()
-st.markdown("#### Connect with me: ")
-st.markdown("Feel free to connect with me through LinkedIn if you have any questions or would like to chat about anything related to ML/AI: [Joan Kusuma](www.linkedin.com/in/joan-kusuma)")
